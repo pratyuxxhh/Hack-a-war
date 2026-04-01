bf1bc5a70af8fec03fe2d2a57c6d9a66543124a3
= <ishu.kool2406@gmail.com>
2026-04-01 21:03:27 +0530

    backend update

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",
+    "http://localhost:5173","http://localhost:5174",
     "http://localhost:3000",
     "http://localhost:5000",
     "http://localhost:5051",
